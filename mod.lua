cdafaa4c8399e642ec7680137c59bff660c67d5c
Anton Menzinsky <anton.menzinsky@asurgent.se>
2025-08-24 22:13:05 +0200

    Initial commit of Decor attach blocks mod with version 0.1.0

---
+++ mod.lua
@@ -0,0 +1,56 @@
+local mod = foundation.createMod();
+
+-- To easily read in and correctly parse the different attach points (Attach_Bottom, Attach_Top etc.)
+mod:registerAssetProcessor("models/Scalable_Attach_Cube.fbx", {
+    DataType = "BUILDING_ASSET_PROCESSOR"
+})
+
+-- Single scaling build part (root, tiling, top)
+mod:registerAssetId("models/Scalable_Attach_Cube.fbx/Prefab/Root_Part", "PREFAB_ROOT_PART")
+mod:registerAssetId("models/Scalable_Attach_Cube.fbx/Prefab/Tiling_Part", "PREFAB_TILING_PART")
+mod:registerAssetId("models/Scalable_Attach_Cube.fbx/Prefab/Top_Part", "PREFAB_TOP_PART")
+
+mod:registerAsset({
+    DataType = "BUILDING",
+    Id = "BLOCK",
+    Name = "BLOCK_NAME",
+    Description = "BLOCK_DESC",
+    BuildingType = "Decoration",
+    AssetCoreBuildingPart = "BLOCK_PART"
+})
+
+-- Scaling cube parts
+
+-- Root (main) part
+mod:registerAsset({
+    DataType = "BUILDING_PART",
+    Id = "BLOCK_PART",
+    ConstructorData = {
+        DataType = "BUILDING_CONSTRUCTOR_SCALER",
+        CoreObjectPrefab = "PREFAB_ROOT_PART",
+        EndPart = "BLOCK_TOP_PART",
+        FillerList = {
+            "BLOCK_TILING_PART"
+        }
+    }
+})
+
+-- Tiling part
+mod:registerAsset({
+    DataType = "BUILDING_PART",
+    Id = "BLOCK_TILING_PART",
+    ConstructorData = {
+        DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
+        CoreObjectPrefab = "PREFAB_TILING_PART"
+    }
+})
+
+-- End part
+mod:registerAsset({
+    DataType = "BUILDING_PART",
+    Id = "BLOCK_TOP_PART",
+    ConstructorData = {
+        DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
+        CoreObjectPrefab = "PREFAB_TOP_PART"
+    }
+})
