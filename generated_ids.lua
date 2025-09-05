d2b84d36acf74f9808c0c266c2d41e60b7c22e1a
Frommarn <17617000+Frommarn@users.noreply.github.com>
2025-09-05 16:20:04 +0200

    Generated files by Foundation when loading the mod

--- generated_ids.lua
+++ generated_ids.lua
@@ -1,25 +1,29 @@
 -- This is a generated file
 -- Do not modify it, unless you know exactly what you are doing
 
-ModId = "bf9b3f8f-d984-4f99-9111-e73e5f6ab063"
+ModId = "c57589c0-75fd-48b4-ac88-a9260f1a7a19"
 
 IdMap = {
-	{ "BLOCK", "BUILDING", "e5f5e53d-377c-4d8d-9c24-5c645327c301" },
-	{ "BLOCK_PART", "BUILDING_PART", "367b5a03-41dc-4129-81e3-211533d8773f" },
-	{ "icons/Icon_Button_Swatch_1.png", "", "6a9fa897-b8fd-474e-99f0-f6334a1e096d" },
-	{ "BLOCK_TILING_PART", "BUILDING_PART", "10b52bec-dcf3-4c66-a50f-75fffee0baf2" },
-	{ "models/Cube Base Color.png", "", "53aa5b16-60ef-4e80-8be5-abffdbc955b0" },
-	{ "BLOCK_TOP_PART", "BUILDING_PART", "2f6560de-550d-4cde-858b-f8babb852802" },
-	{ "EVENT_BLOCK_UNLOCK", "EVENT", "471be370-f826-40fa-afb5-ea2a4dbb59ef" },
-	{ "models/Scalable_Attach_Cube.fbx", "", "d1ab4331-1f06-45ec-b651-60b3fd87998f" },
-	{ "models/Tex_Wall_Stone_Rustic_01A_Diff.png", "", "6812947e-0e24-4c7f-a22c-84d302455c31" },
-	{ "models/Block Base Color.png", "", "a498b521-4832-4850-8cdc-9dae8af27af8" },
-	{ "BLOCK_MATERIAL_SET_LIST", "MATERIAL_SET_LIST", "4669c62f-f1f3-451a-9889-8e8cd90a0ed6" },
-	{ "icons/Icon_Button_Swatch_2.png", "", "a819f8e5-ccd7-46d2-8abb-481bb2a59409" },
-	{ "icons/Icon_Button_Swatch_3.png", "", "a643be8a-f882-423f-8b00-daba45bd9ac6" },
-	{ "icons/Icon_Button_Swatch_4.png", "", "291a70d0-194d-46c1-93f7-7546457b8f44" },
-	{ "icons/Icon_Button_Swatch_5.png", "", "61dbc784-0f24-4042-a43c-0652addf879a" },
-	{ "icons/Icon_Button_Swatch_6.png", "", "0da79e56-d4b0-4c31-9f9f-7eb97b06d247" },
-	{ "icons/Icon_Button_Swatch_7.png", "", "64937b96-557b-4788-ac10-7ad18afafd66" },
-	{ "icons/Icon_Button_Swatch_8.png", "", "c4ddf65c-aba8-4a5c-a166-4cf3ec283ba2" },
+	{ "BLOCK_MATERIAL_SET_LIST", "MATERIAL_SET_LIST", "fada84f4-a63e-4a82-8380-cd7708085b36" },
+	{ "BLOCK", "BUILDING", "d71f2469-50e9-4c28-bcf3-32de01f0d476" },
+	{ "BLOCK_DOUBLE_ROOT_ONE_PART", "BUILDING_PART", "c05b43e7-b0b3-4d33-9be5-961dbc0ab233" },
+	{ "BLOCK_TILING_PART", "BUILDING_PART", "c81859a8-6d3f-4d54-8cc5-63ffaa0db765" },
+	{ "BLOCK_DOUBLE", "BUILDING", "2300139a-b454-4138-8a9b-00b312eaa199" },
+	{ "BLOCK_DOUBLE_TILING_TWO_PART", "BUILDING_PART", "2b34aa1d-3883-4585-8c80-c27b4a8c1c28" },
+	{ "BLOCK_ROOT_PART", "BUILDING_PART", "5c33852d-89ac-411f-8d27-52386895eed9" },
+	{ "BLOCK_TOP_PART", "BUILDING_PART", "44f237e6-f021-4af0-84c4-3a337f56d559" },
+	{ "BLOCK_DOUBLE_TILING_ONE_PART", "BUILDING_PART", "dabec54c-0a5c-4d18-9d13-aead981aeec6" },
+	{ "BLOCK_DOUBLE_ROOT_TWO_PART", "BUILDING_PART", "61a2eb34-9743-490a-876b-a48072fba17c" },
+	{ "icons/Icon_Button_Swatch_3.png", "", "5cac5c55-1eef-4c4a-90c2-ba39d944122b" },
+	{ "BLOCK_DOUBLE_TOP_PART", "BUILDING_PART", "36c7becc-c492-4d45-ac76-11e8f2ca0dd3" },
+	{ "EVENT_BLOCK_UNLOCK", "EVENT", "cf775aa0-a5a3-4679-bb5b-185168b47b8a" },
+	{ "icons/Icon_Button_Swatch_1.png", "", "1e664694-125c-4896-a9bc-237649aa651c" },
+	{ "icons/Icon_Button_Swatch_2.png", "", "76fb340a-165d-4071-b814-b19ab465d68d" },
+	{ "icons/Icon_Button_Swatch_4.png", "", "aa80c443-343f-4adf-9289-5b1faa1d1a1e" },
+	{ "icons/Icon_Button_Swatch_5.png", "", "e5b830aa-aee9-4811-9c27-4e28c9b4d142" },
+	{ "icons/Icon_Button_Swatch_6.png", "", "61b84196-1e12-4fdf-86ab-51307c014ec1" },
+	{ "icons/Icon_Button_Swatch_7.png", "", "56133a14-a751-4288-bf5b-653e4fb58e73" },
+	{ "icons/Icon_Button_Swatch_8.png", "", "d13c1e3c-e7c1-44dd-ae85-bc209c1d5a97" },
+	{ "models/Block Base Color.png", "", "973b9a18-447a-409f-b9d8-955a8cd848b4" },
+	{ "models/Scalable_Attach_Cube.fbx", "", "3b9e954b-74a1-4ddf-9d50-0a07f2227d37" },
 }
